164122d19fa6f7e13aa9268a851239579a632b59
Peter Milne <helipilot50@gmail.com>
2024-08-29 17:34:27 +0200

    started reporting test

---
+++ part_3/src/part_3/tests/test_reports.py
@@ -0,0 +1,41 @@
+from part_3.tools.accounts import AccountsTool
+from part_3.tools.campaigns import CampaignsTool
+from part_3.tests.utils import fetchToken
+from part_3.tests.utils import write_data
+
+from part_3.tools.analytics import CampaignAnalyticsTool
+
+
+def test_create_impressions_report():
+    token = fetchToken()
+    assert token is not None
+
+    # tools
+    accounts = AccountsTool(token=token)
+    campaigns = CampaignsTool(token=token)
+
+    # accounts
+    accounts_api_result = accounts._run()
+    assert accounts_api_result is not None
+    assert accounts_api_result["data"] is not None
+    assert len(accounts_api_result["data"]) > 0
+    account_id = accounts_api_result["data"][0]["id"]
+    assert account_id is not None
+
+    # campaigns for first account
+    campaigns_api_result = campaigns._run(accountId=account_id)
+    assert campaigns_api_result is not None
+    assert campaigns_api_result["data"] is not None
+    assert len(campaigns_api_result["data"]) > 0
+    campaign_list = campaigns_api_result["data"]
+    campaign_ids = [campaign["id"] for campaign in campaign_list]
+
+    impressions_tool = CampaignAnalyticsTool(token=token)
+    impressions_api_result = impressions_tool._run(
+        campaignIds=campaign_ids, startDate="2020-01-01", endDate="2020-12-31"
+    )
+    assert impressions_api_result is not None
+    assert impressions_api_result["data"] is not None
+    assert len(impressions_api_result["data"]) > 0
+    data = impressions_api_result["data"]
+    write_data(data, "test_impressions.json")
---
+++ part_3/src/part_3/tests/utils.py
@@ -0,0 +1,69 @@
+from part_3.tools.auth import AuthTool
+import os
+import json
+
+output_directory = "output"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+
+def full_file_path(file_name):
+    return f"{output_directory}/{file_name}"
+
+
+def write_data(data, file_name):
+    file_name = full_file_path(file_name)
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    with open(file_name, "w") as file:
+        file.write(json.dumps(data))
+
+
+def fetchToken():
+    auth = AuthTool()
+    response = auth._run()
+    return response["access_token"]
+
+
+def attrubtes_only(li):
+    return li["attributes"]
+
+
+def start_date(li):
+    return li["startDate"]
+
+
+def end_date(li):
+    return li["endDate"]
+
+
+def budget(li):
+    if li["budget"]:
+        return li["budget"]
+    else:
+        return 0
+
+
+def date_budget(li):
+    return {"date": start_date(li), "budget": budget(li)}
+
+
+def date(li):
+    return li["date"]
+
+
+def auction(li):
+    return li["auction"]
+
+
+def preferred(li):
+    return li["preferred"]
+
+
+def money(li):
+    return li["budget"]
+
+
+def short_date(date: str):
+    return date[:7]
