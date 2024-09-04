60a009fad8bcc9dcc8f692c12d3a786db487a25b
Peter Milne <helipilot50@gmail.com>
2024-09-04 11:02:39 +0200

    Detailed comments updated models

--- part_1/src/part_1/crew.py
+++ part_1/src/part_1/crew.py
@@ -1,8 +1,17 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 from part_1.tools.auth import AuthTool
 from part_1.tools.accounts import AccountsTool, RetailersTool, BrandsTool
+
+import os
+
+# For local testing using ollama. WARNING: Unless you have a powerful GPU, this will be VERY VERY slow.
+# os.environ["OPENAI_API_KE"]='NA' # No API Key required for Ollama
+# ollm = ChatOllama(
+#     model = "llama3.1",
+#     base_url = "http://localhost:11434")
 
 auth = AuthTool()
 auth_response = auth._run()
@@ -16,6 +25,10 @@ class Part1Crew:
     agents_config = "config/agents.yaml"
     tasks_config = "config/tasks.yaml"
 
+    """
+    Account manager agent instance created from the config file.
+    The function is decorated with the @agent decorator to indicate that it is an agent.
+    """
     @agent
     def account_manager(self) -> Agent:
         return Agent(
@@ -25,14 +38,26 @@ class Part1Crew:
                 RetailersTool(token=token),
                 BrandsTool(token=token),
             ],
+            # For local testing using ollama"
+            # llm=ollm
         )
 
+    """
+    Accounts task instance created from the config file.
+    This function is decorated with the @agent decorator to indicate that it is an agent.
+    It's job is to retrive Accounts data and produce a Markdown file.
+    """
     @task
     def accounts(self) -> Task:
         return Task(
-            config=self.tasks_config["accounts"], output_file="output/accounts.md"
+            config=self.tasks_config["accounts"], output_file="output/accounts.md",
         )
 
+    """
+    Brands task instance created from the config file.
+    This function is decorated with the @agent decorator to indicate that it is an agent.
+    It's job is to retrive Brands data for a specific Account and produce a Markdown file.
+    """
     @task
     def brands(self) -> Task:
         return Task(
@@ -40,20 +65,21 @@ class Part1Crew:
             output_file="output/brands.md",
         )
 
+    """
+    Retailers task instance created from the config file.
+    This function is decorated with the @agent decorator to indicate that it is an agent.
+    It's job is to retrive Retailers data for a specific Account and produce a Markdown file.
+    """
     @task
     def retailers(self) -> Task:
         return Task(
             config=self.tasks_config["retailers"],
             output_file="output/retailers.md",
         )
-
-    @task
-    def analytics(self) -> Task:
-        return Task(
-            config=self.tasks_config["analytics"],
-            output_file="output/analytics.md",
-        )
-
+    """
+    This function creates  the crew! It is decorated with the @crew decorator to indicate that it is a crew.
+    The crew orchestrates the agents and tasks to complete the process.
+    """
     @crew
     def crew(self) -> Crew:
         """Creates the Part 1 crew"""
@@ -64,6 +90,8 @@ class Part1Crew:
             verbose=True,
             memory=True,
             planning=True,
-            planning_llm=ChatOpenAI(model="gpt-3.5-turbo"),
+            # For local testing using ollama"
+            #planning_llm=ollm,
+            planning_llm=ChatOpenAI(model="gpt-4o-mini"),
             output_log_file="output/part_1.log",
         )
