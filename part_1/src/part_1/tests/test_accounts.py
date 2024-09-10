ec622bfe0f7b0cce4f495caebc9ddd0f6ab2e920
Peter Milne <helipilot50@gmail.com>
2024-09-10 11:23:10 +0200

    refactored tests

--- part_1/src/part_1/tests/test_accounts.py
+++ part_1/src/part_1/tests/test_accounts.py
@@ -1,42 +1,37 @@
 import os
 import json
-from part_1.tests.utils import fetchToken
 from part_1.tools.accounts import AccountsTool, BrandsTool, RetailersTool
+from crewai_tools import (
+    FileWriterTool,
+    FileReadTool,
+    DirectoryReadTool,
+    DirectorySearchTool,
+)
 
-output_directory = "output"
-
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
-
-
-def write_data(data, file_name):
-    file_name = f"{output_directory}/{file_name}"
-    if os.path.exists(file_name):
-        os.remove(file_name)
-    with open(file_name, "w") as file:
-        file.write(json.dumps(data))
+accounts = AccountsTool()
+brands = BrandsTool()
+fileWriter = FileWriterTool()
+retailers = RetailersTool()
 
 
 def test_accounts():
 
-    token = fetchToken()
-    assert token is not None
-
-    accounts = AccountsTool(token=token)
     accounts_api_result = accounts._run()
     assert accounts_api_result is not None
     assert accounts_api_result["data"] is not None
     assert len(accounts_api_result["data"]) > 0
     data = accounts_api_result["data"]
-    write_data(data, "test_accounts.json")
+    fileWriter._run(
+        directory="output",
+        filename=f"test_accounts.json",
+        overwrite=True,
+        content=json.dumps(data),
+    )
+    assert os.path.exists("output/test_accounts.json")
 
 
 def test_accounts_brands():
 
-    token = fetchToken()
-    assert token is not None
-
-    accounts = AccountsTool(token=token)
     accounts_api_result = accounts._run()
     assert accounts_api_result is not None
     assert accounts_api_result["data"] is not None
@@ -45,21 +40,22 @@ def test_accounts_brands():
     account_id = accounts_api_result["data"][0]["id"]
     assert account_id is not None
 
-    brands = BrandsTool(token=token)
     brands_api_result = brands._run(accountId=account_id)
     assert brands_api_result is not None
     assert brands_api_result["data"] is not None
     assert len(brands_api_result["data"]) > 0
     data = brands_api_result["data"]
-    write_data(data, "test_brands.json")
+    fileWriter._run(
+        directory="output",
+        filename=f"test_brands.json",
+        overwrite=True,
+        content=json.dumps(data),
+    )
+    assert os.path.exists("output/test_brands.json")
 
 
 def test_accounts_retailers():
 
-    token = fetchToken()
-    assert token is not None
-
-    accounts = AccountsTool(token=token)
     accounts_api_result = accounts._run()
     assert accounts_api_result is not None
     assert accounts_api_result["data"] is not None
@@ -68,10 +64,15 @@ def test_accounts_retailers():
     account_id = accounts_api_result["data"][0]["id"]
     assert account_id is not None
 
-    retailers = RetailersTool(token=token)
     retailers_api_result = retailers._run(accountId=account_id)
     assert retailers_api_result is not None
     assert retailers_api_result["data"] is not None
     assert len(retailers_api_result["data"]) > 0
     data = retailers_api_result["data"]
-    write_data(data, "test_retailers.json")
+    fileWriter._run(
+        directory="output",
+        filename=f"test_retailers.json",
+        overwrite=True,
+        content=json.dumps(data),
+    )
+    assert os.path.exists("output/test_retailers.json")
--- part_1/src/part_1/tests/utils.py
+++
@@ -1,8 +0,0 @@
-from part_1.tools.auth import AuthTool
-
-
-def fetchToken():
-    auth = AuthTool()
-    response = auth._run()
-    print("response", response)
-    return response["access_token"]
